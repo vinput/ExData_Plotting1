d61c025decf5f5d7b6fcbf67314e53359bd57e56
Vinod Puthukkudy <vinput@gmail.com>
2015-11-08 00:28:34 -0500

    R Codes - Load_data Plot 1 Plot 2  Plot 3 & Plot 4

---
+++ load_data.R
@@ -0,0 +1,18 @@
+setwd("C:/ExploratoryDataAnalysis")
+filename <- "household_power_consumption.txt"
+data <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) # 2075259 9
+attach(data)
+# We will only be using data from the dates 2007-02-01 and 2007-02-02
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
